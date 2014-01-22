ee6a5063caa58c384b5a9d0f5cd6b691befd20dc
vagrant <vagrant@precise32.(none)>
2014-01-22 23:51:25 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,52 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Meow");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+	$("#submitBtn").click(changeProject);
+}
+
+function projectClick(e) {
+	// prevent the page from reloading
+	e.preventDefault();
+	// In an event handler, $(this) refers to the object that triggered the event
+	//$(this).css("background-color", "#7fff00");
+
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       $(containingProject).fadeOut();
+    }
+}
+
+function changeProject(e) {
+	e.preventDefault();
+
+	var projectID = $("#project").val();
+	var description = $("#description").val();
+
+	$(projectID).animate({
+		width: $("#width").val()
+	});
+	
+
+	if (description.length != 0) { 
+		if ($(projectID).find(".project-description").length == 0) // If there isn't any descriptions
+			$(projectID).append("<div class='project-description'><p>" + description + "</p></div>");
+		else // This will replace the old description with the new one
+			$(projectID).find(".project-description").html("<p>" + description + "</p>");
+	}
 }
